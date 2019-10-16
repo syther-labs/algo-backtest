963cf7a24a6eb8597c9d6f52a399895ea5c36644
Michael Roher <roher4@gmail.com>
2019-10-16 03:38:04 -0400

    Add spec for abstract node + monkey patch enumerable

---
+++ spec/engine/tree/abstract_node_spec.cr
@@ -0,0 +1,30 @@
+require "../../spec_helper"
+
+# We create this class to test the abstract node properties
+class MockNode < Algo::Backtester::AbstractNode; end
+
+describe Algo::Backtester::AbstractNode do
+  describe "#name" do
+    it "should return the name of the node" do
+      node = MockNode.new(name: "sample-node-name")
+      node.name.should eq("sample-node-name")
+    end
+  end
+
+  describe "#root" do
+    it "should return if the node is the root node" do
+      is_root = MockNode.new(name: "parent", root: true)
+      is_child = MockNode.new(name: "child", root: false)
+      is_root.root.should be_true
+      is_child.root.should be_false
+    end
+    it "should have a setter to modify the root property" do
+      node = MockNode.new(name: "foo", root: false)
+      node.root.should be_false
+      # Then we apply the property
+      node.root = true
+      node.root.should be_true
+    end
+  end
+  pending "to test children"
+end
---
+++ src/util/enumerable_stat_patches.cr
@@ -0,0 +1,20 @@
+module Enumerable
+  def sum
+    return 0 if self.empty?
+    return self.reduce(&.+)
+  end
+
+  def mean
+    return self.sum / self.size.to_f
+  end
+
+  def variance
+      avg = self.mean
+      mean_squares = self.map { |el| (el - avg) ** 2 }
+      return mean_squares.reduce(&.+) / self.size
+  end
+
+  def stddev
+    return Math.sqrt(self.variance)
+  end
+end
