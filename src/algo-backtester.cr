b7e01b634b3f09d26c35656ad6f0c1e50909ec90
Michael Roher <roher4@gmail.com>
2019-10-29 00:02:35 -0400

    main: remove testing example from main file

--- src/algo-backtester.cr
+++ src/algo-backtester.cr
@@ -14,31 +14,4 @@ module AlgoBacktester
   include AlgoBacktester::StockExchange
 
   VERSION = "0.1.0"
-
-  bars = [] of Bar
-  # To refresh add :record
-  load_cassette("appl-bars-aug-to-oct") do
-    bars = TiingoDataDownloader.new.query("AAPL", 3.months.ago, Time.now)
-  end
-
-  data = DataHandler.new
-  data.stream = bars
-
-  strategy = Strategy.new("buy_and_hold")
-  strategy.set_algo(
-    RunMonthlyAlgorithm.new,
-    SignalAlgorithm.new(direction: :buy)
-  )
-  strategy.add_child Asset.new("AAPL")
-
-  backtest = Backtest.new(initial_cash: 1000.0_f64, data: data, strategy: strategy)
-
-  backtest.run
-
-  backtest.statistics.print_summary
-
-  # result, error =  SMA.new(period: 3).run(strategy)
-  # puts [result, error]
 end
-
-puts [1, 2, 3, 4].stddev
